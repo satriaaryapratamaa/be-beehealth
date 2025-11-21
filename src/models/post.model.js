d4e207978110c9f1d525642243c46d5a41f11a68
satriaaryapratamaa <aryaprtx@gmail.com>
2025-11-21 14:24:11 +0700

    add post model

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -5,8 +5,91 @@ export const create = (userId, deskripsi, imageURL) => {
         data: { 
             userId: userId,
             deskripsi: deskripsi,
-            imageURL: imageURL || null
+            imageURL: imageURL || null,
         },
     });
 }
 
+export const findAll = () => {
+    return prisma.post.findMany({
+        orderBy: {
+            sharedDate: 'desc',
+        },
+        include: {
+            user: {
+                select: {
+                    id: true,
+                    username: true,
+                    nama: true,
+                },
+            },
+        },
+        Comment: {
+            include: {
+                user: {
+                    select: {
+                        id: true,
+                        username: true,
+                    },
+                },
+            },
+            orderBy: {
+                date: 'desc',
+            },
+        },
+    });
+};
+
+export const remove = (id) => {
+    return prisma.post.delete({
+        where: { id: id },
+    });
+};
+
+export const toggleLike = async (userId, postId) => {
+    const existingLike = await prisma.like.findUnique({
+        where: {
+            userId_postId: { 
+            userId: userId,
+            postId: postId,
+            },
+        },
+    });
+
+    if (existingLike) {
+    
+        await prisma.like.delete({
+            where: { id: existingLike.id },
+        });
+        return { action: 'unliked' };
+    } else {
+    
+        const newLike = await prisma.like.create({
+            data: {
+                userId: userId,
+                postId: postId,
+            },
+        });
+        return { action: 'liked', data: newLike };
+    }
+};
+
+
+export const createComment = (userId, postId, text) => {
+    return prisma.comment.create({
+        data: {
+        userId,
+        postId,
+        text,
+        },
+    include: { 
+        user: {
+            select: {
+            id: true,
+            nama: true,
+            username: true
+            }
+        }
+        }
+    });
+};
