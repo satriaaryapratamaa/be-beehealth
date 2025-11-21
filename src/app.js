c301cc72fe1edd484fd012c58ace4ddad3178c76
satriaaryapratamaa <aryaprtx@gmail.com>
2025-11-21 14:45:57 +0700

    update app.js

--- src/app.js
+++ src/app.js
@@ -18,9 +18,17 @@ export function createApp(prisma) {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
+    // app.use((req, res, next) => {
+    //     req.prisma = prisma;
+    //     next();
+    // }); 
+    
     app.use((req, res, next) => {
         req.prisma = prisma;
-        next();
+        res.header['Access-Control-Allow-Origin'] = '*';
+        res.header['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+        res.header['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+        next();     
     });
 
     app.get('/api', (req, res) => {
