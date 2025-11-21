68642dd30d792e4d4a6a102e80fb9dfd836695f2
Fanz27 <fatihnovendi86@gmail.com>
2025-11-21 15:10:40 +0700

    test header

--- src/app.js
+++ src/app.js
@@ -10,11 +10,12 @@ export function createApp(prisma) {
     const app = express();
 
     app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+        origin: ['http://localhost:5173', 'http://localhost:3000'],
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning']
+    }));
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
